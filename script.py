24e3d97ef0646c6f0ebf46b2a10bce12c5ff90d1
Sergio Domínguez <sergio.dmgz@gmail.com>
2017-04-10 03:00:30 +0200

    Un programita que abre ventanas

--- script.py
+++ script.py
@@ -1,1 +1,17 @@
-# hola
+import webbrowser as WebBrowser
+
+# MacOS
+# chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+# Windows
+# chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+# Linux
+# chrome_path = '/usr/bin/google-chrome %s'
+
+chrome = WebBrowser.get('windows-default')
+
+chrome.open('http://www.youtube.com/')
+chrome.open('http://www.finofilipino.org/')
+chrome.open('http://www.reddit.com/')
+chrome.open_new('http://www.gmail.com/')
